ef766a32f73329038703a0a4d104364a082c8588
Tomaspaez809 <ryudetsuki1587@gmail.com>
2025-01-10 11:43:22 -0400

    fix: update install.sh script with changes to package installation and plugin setup

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+## Distro basadas en debian o package manager basado en apt ---> :D
+
 # add my user to sudoers file
 # install the programs that need it
 sudo apt-get update -y 
-sudo apt install curl wget unzip zip gnupg2  snapd nvim kitty zsh flatpak plasma-discover-backend-flatpak
+sudo apt install curl git wget unzip zip gnupg2 snapd nvim kitty zsh flatpak 
 
 # change default shell to zsh
 chsh -s $(which zsh)
@@ -18,11 +20,10 @@ sudo mv -f .zshrc ~/.zshrc
 # install  plugins de ohmyzsh
 
 #zsh- autosuggestion
-git clone https://github.com/zsh-users/zsh-autosuggestions && ${~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 #zsh-syntax-highlighting
 sudo apt install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${$HOME}/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # install github-cli (gh)
 sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
@@ -38,9 +39,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 # install  nodejs and npm with nvm
 nvm install --lts
+nvm install 16.0.0
+nvm install 14.5.0
 
 # get repos
 
-kitty -e ./get_repos.sh RyuTsuki08
+./get_repos.sh RyuTsuki08
 
 
